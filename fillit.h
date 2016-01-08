d373d3b9ef84a51f22bdde5b4d25c1e4f9a47d30
Arthur TOUPART <atoupart@e3r5p19.42.fr>
2016-01-08 17:31:20 +0100

    fillit.h

--- fillit.h
+++ fillit.h
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/08 17:13:24 by atoupart          #+#    #+#             */
-/*   Updated: 2016/01/08 17:30:17 by atoupart         ###   ########.fr       */
+/*   Created: 2016/01/08 17:28:17 by atoupart          #+#    #+#             */
+/*   Updated: 2016/01/08 17:30:10 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
-
-#define BUF_SIZE 21
-
-int		main(int argc, char **argv)
-{
-	int fd;
-	int ret;
-	char buf[BUF_SIZE + 1];
-
-	if ((fd = open(argv[1], O_RDONLY)) == -1)
-	{
-
-	return (0);
-}
+#include "libft.h"
