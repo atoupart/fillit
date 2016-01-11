7cb1e0e48ae8541fc6842702d87043763d5e1034
Arthur TOUPART <atoupart@e3r7p17.42.fr>
2016-01-11 21:20:03 +0100

    blabla

---
+++ ft_openfile_checker.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_openfile_checker.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/11 14:22:06 by atoupart          #+#    #+#             */
+/*   Updated: 2016/01/11 19:58:02 by atoupart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+#define BUF_SIZE 21
+
+static void		ft_checker(char *buf, int ret)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+
+	if (ret != 20 && ret != 21)
+		ft_error();
+
+	while (buf[i])
+	{
+		if (buf[i] != '.' && buf[i] != '#' && buf[i] != '\n' && buf[i] != 'a')
+		{
+			ft_putchar(buf[i]);
+			ft_putchar('\n');
+			ft_putstr("nature");
+			ft_error();
+		}
+		i++;
+	}
+
+	i = 4;
+	j = 0;
+	while (j != 4)
+	{
+		if (buf[i] != '\n')
+		{
+			ft_putnbr(j);
+			ft_putchar('\n');
+			ft_putnbr(i);
+			ft_putchar('\n');
+			ft_putstr("forme");
+			ft_error();
+		}
+		i = i + 5;
+		j++;
+	}
+	if (buf[ret] != '\n')
+		ft_error();
+}
+
+void			ft_openfile_checker(char *str)
+{
+	int fd;
+	int ret;
+	char buf[BUF_SIZE + 1];
+
+	if ((fd = open(str, O_RDONLY)) == -1)
+		ft_error();
+	while ((ret = read(fd, buf, BUF_SIZE)))
+	{
+		buf[ret] = '\0';
+		ft_checker(buf, ret);
+	}
+	if (close(fd) == -1)
+		ft_error();
+}
