a3546fcb12ca70f7950f48dce6ba29a026fecd24
atoupart <toupart.arthur@hotmail.fr>
2016-01-18 14:18:56 +0100

    ajout checkform

--- srcs/ft_checkform.c
+++ srcs/ft_checkform.c
@@ -1,22 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_splitncheck.c                                   :+:      :+:    :+:   */
+/*   ft_checkform.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/15 11:52:37 by atoupart          #+#    #+#             */
-/*   Updated: 2016/01/15 11:54:16 by atoupart         ###   ########.fr       */
+/*   Created: 2016/01/18 12:21:10 by atoupart          #+#    #+#             */
+/*   Updated: 2016/01/18 14:16:32 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char		**ft_splitncheck(char *str, t_tool *data)
+void		ft_checkform(char *buf, t_tool *data)
 {
-	char **tab;
-
-	tab = ft_strsplit(str, '\n');
-	ft_checktab(tab, data);
-	return (tab);
+	X = 4;
+	Y = 20;
+	I = 0;
+	while (buf[X])
+	{
+		if (buf[X] != '\n')
+			ft_error();
+		if (I == 3)
+		{
+			X++;
+			I = -1;
+		}
+		else if (buf[X + 1] != '\0')
+		{
+			X += 5;
+			I++;
+		}
+	}
+	while (buf[Y])
+	{
+		if (buf[Y] != '\n')
+			ft_error();
+		if (buf[Y + 20] != '\0')
+			Y += 21;
+	}
 }
--- srcs/ft_openfile_checker.c
+++ srcs/ft_openfile_checker.c
@@ -6,7 +6,7 @@
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/15 11:55:04 by atoupart          #+#    #+#             */
-/*   Updated: 2016/01/15 11:58:24 by atoupart         ###   ########.fr       */
+/*   Updated: 2016/01/18 13:35:44 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,9 @@ char		**ft_openfile_checker(char *str, t_tool *data)
 		ft_error();
 	ret = read(fd, buf, BUF_SIZE);
 	buf[ret] = '\0';
-	tab = ft_splitncheck(buf, data);
+	ft_checkform(buf, data);
+	tab = ft_strsplit(buf, '\n');
+	ft_checktab(tab, data);
 	if (close(fd) == -1)
 		ft_error();
 	return (tab);
