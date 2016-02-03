0a63cab39a96a1d19333fe6b1e7cc94439aa1a92
Arthur TOUPART <atoupart@e3r7p23.42.fr>
2016-02-03 11:37:46 +0100

    push push

--- src/put_piece.c
+++ src/put_piece.c
@@ -6,7 +6,7 @@
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 13:05:40 by atoupart          #+#    #+#             */
-/*   Updated: 2016/02/02 17:37:13 by atoupart         ###   ########.fr       */
+/*   Updated: 2016/02/03 10:58:46 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		put_piece(t_tool *data)
 	while (TAB[I][++K])
 	{
 		J = -1;
-		while (TAB[I][K][++J] && (K + Y < SQUARE) && (J + X < SQUARE))
+		while (TAB[I][K][++J])// && (K + Y < SQUARE) && (J + X < SQUARE))
 		{
 			if (TAB[I][K][J] == '#')
 				FINALTAB[Y + K][X + J] = 'A' + I;
--- src/recursive_try.c
+++ src/recursive_try.c
@@ -6,7 +6,7 @@
 /*   By: atoupart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 13:05:40 by atoupart          #+#    #+#             */
-/*   Updated: 2016/02/02 17:32:42 by atoupart         ###   ########.fr       */
+/*   Updated: 2016/02/03 11:35:30 by atoupart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,61 @@
  */
 int		recursive_try(t_tool *data)
 {
+	ptn(1);pts("  ################   Debut recursive  I = ");ptn(I);ptcn;
 	if (I == NBTETRI)
+	{
+		ptn(2);pts("I == NBTETRI, return (1)");ptcn;
 		return (1);
+	}
 	Y = -1;
 	while (++Y < SQUARE)
 	{
+		ptn(3);pts("  ++Y  :  ");ptn(Y);ptcn;
 		X = -1;
 		while (++X < SQUARE)
 		{
+			ptn(4);pts("  ++X  :  ");ptn(X);ptcn;
 			if (ft_verif_put(data) == 1)
 			{
 				I++;
+				ptn(5);pts("  verif & put ok, I++  :  ");ptn(I);ptcn;
 				if (recursive_try(data) == 0)
 				{
 					retire_piece(data);
 					I--;
+					ptn(6);pts("  recursive == 0, retire piece et I--");ptcn;
 				}
 				else
+				{
+					ptn(7);pts("  recursive != 0, return (1)");ptcn;
 					return (1);
+				}
 			}
 		}
 	}
 	if (I == 0)
 	{
+		ptn(8);pts("  I == 0, square++ free init et recursive");ptcn;
 		SQUARE += 1;
 		free_square(data);
 		init_square(data);
 		recursive_try(data);
 	}
+	ptn(9);pts("  I != 0, return (0)");ptcn;
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
