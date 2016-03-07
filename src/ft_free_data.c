f0df5eefa3beaa59f3b2378148b067f4256eee9a
Emilien BAUDET <ebaudet@e1r4p7.42.fr>
2016-03-07 19:13:55 +0100

    42 filechecker good

--- src/ft_free_data.c
+++ src/ft_free_data.c
@@ -6,42 +6,32 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/31 21:05:57 by ebaudet           #+#    #+#             */
-/*   Updated: 2016/02/04 17:43:17 by atoupart         ###   ########.fr       */
+/*   Updated: 2016/03/07 19:11:33 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static void		ft_free_3D_tab(char ***tab, t_tool *data)
+static void		ft_free_3d_tab(char ***tab, t_tool *data)
 {
 	I = -1;
 	while (tab && tab[++I])
 	{
 		Y = -1;
 		while (tab[I] && tab[I][++Y])
-		{
-			ft_putstr("free(tab[I][Y])\n");
 			free(tab[I][Y]);
-		}
-		if (tab[I]) {
-			ft_putstr("free(tab[I])\n");
+		if (tab[I])
 			free(tab[I]);
-		}
-		ft_putstr("couocu\n");
 	}
-	ft_putstr("free(tab)\n");
 	free(tab);
 }
 
-void		ft_free_data(t_tool *data)
+void			ft_free_data(t_tool *data)
 {
 	if (data)
 	{
-		ft_putstr("free(TABTETRI)\n");
-		ft_free_3D_tab(TABTETRI, data);
-		ft_putstr("free(TAB)\n");
-		ft_free_3D_tab(TAB, data);
-		ft_putstr("free(data)\n");
+		ft_free_3d_tab(TABTETRI, data);
+		ft_free_3d_tab(TAB, data);
 		free(data);
 	}
 }
